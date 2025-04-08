9d53f6c2815ab66cabcae26c60839725103a0326
OtonielCarreon <ocarreon24@gmail.com>
2025-04-08 13:59:00 -0700

    convert to reactJS
    I am attempting to create my small website into a more modern reactJS web application

---
+++ components/blog/BlogPost.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogPost = () => {
+    <html>
+        <p><h1>big words</h1></p>
+    </html>
+}
---
+++ wots.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import BlogPost from "componenets/BlogPost";
+
+const WOTS = () => {
+    <html>
+        <title>Word on the Street</title>
+        <link rel="stylesheet" href="styles.css"></link>
+        <body>
+        <div class="header-2">
+            <a href="index.html"><img src="images\arrow.png" alt="arrow"></img></a>
+            <h3>Word on the Street</h3>
+        </div>
+        <div class="content">
+
+        </div>
+        </body>
+    </html>
+}
